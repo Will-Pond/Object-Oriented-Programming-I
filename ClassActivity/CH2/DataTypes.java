afe90cac5331f397c2daa76879ebe09f9b506f31
Will-Pond <106034736+Will-Pond@users.noreply.github.com>
2025-01-03 14:39:22 -0500

    Add files via upload

---
+++ ClassActivity/CH2/DataTypes.java
@@ -0,0 +1,43 @@
+
+public class DataTypes {
+
+    public static void testPrimitiveTypes() {
+        int x = 10;
+        System.out.println("1: " + (x == ?????));
+        
+
+        x = Integer.MIN_VALUE; // What is the value of Integer.MIN_VALUE?
+        // You can execute this command to get an answer and fill in the ????? -> System.out.println(x);
+        System.out.println("2: " + (x == ?????));
+
+        long x2 = 1234567890123L;
+        System.out.println("3: " + (x2 == ?????));
+
+        double y = 10.1;
+        System.out.println("4: " + (y == 10.1)); // This returns true, but dangerous, think why?
+
+        float y2 = 123232.1234567890123456F; // Don't forget the F for float type data
+        System.out.println("5: " + (y2 == 123232.1234F)); // This returns, true, but You see why it is dangerous
+        
+        boolean z = true;
+        System.out.println("6: " + (z == true));
+    }
+
+    public static void testCasting() {
+        int x = 10;
+        int y = 20;
+        System.out.println("7: " + (0.5 != x / y));
+        System.out.println("8: " + (0.5 == (double) x / y));
+    }
+
+
+    public static void main(String[] args) {
+        testPrimitiveTypes();
+        testCasting();
+    }
+}
+
+/** Copy your output here - You should have 6 trues
+ *
+
+ */
---
+++ ClassActivity/CH2/Process.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/* 
+	Open Ch2 project to copy the Process.java into the src directory. 
+	The code should do the following actions (statements).
+		1. You get an input from an user and assign the value to int type variable x.
+		2. You get another input from an user and assign it to int type variable y.
+		3. You add x and y to assign the result to a variable z. 
+		4. Print out the z value in "z = 10" when z is the sum
+
+Refer to page 35 of pdf slides. 
+*/
+
+?????
+
+/** Copy your output here
+ *
+
+ */
